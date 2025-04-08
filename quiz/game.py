1f3914d637191a1459284ddadb2b577b010f0f10
navaf <navafsoft@gmail.com>
2025-04-08 17:21:46 +0530

    Shuffle questions before starting the quiz

--- quiz/game.py
+++ quiz/game.py
@@ -1,9 +1,12 @@
 from quiz.questions import questions
 from quiz.utils import ask_question
+import random
 
 def start_quiz():
     print("Welcome to My Python Quiz Game :D!\n")
     score = 0
+
+    random.shuffle(questions)
 
     for q in questions:
         correct = ask_question(q)
